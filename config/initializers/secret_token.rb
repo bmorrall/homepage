034b4b4f1c15c31e59c6331ea0bf3537a4f1ee30
Ubuntu <ubuntu@ip-10-240-18-160.ap-southeast-2.compute.internal>
2012-12-10 11:44:08 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Benmorrall::Application.config.secret_token = '64daf325b53cb8b9e06c33935782c65c8b71838f2a5409d06248c5bc8562c5ab9fafe6dfd9048ce652b527bb2a019e86eee95b4d1beeb169920de4fcf1db1a47'

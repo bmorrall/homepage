3b5312680022d51741e3777d1e54ba8ffac365a8
Ben Morrall <bemo56@hotmail.com>
2012-12-10 12:20:08 +0000

    Added Cookie Clearing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  protected
+
+  # Removes Session Cookies
+  def clear_session!
+    session.clear
+  end
+
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
+  after_filter :clear_session!
+
   def index
     render :layout => nil
   end

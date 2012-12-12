db38b003afb20c7b51b7946cd8cb5524f5f4211f
Ben Morrall <bemo56@hotmail.com>
2012-12-12 14:14:00 +0000

    Set asset_host to subdomain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Benmorrall::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "http://assets.benmorrall.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )

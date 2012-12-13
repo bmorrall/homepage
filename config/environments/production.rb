b927f2cdc80c649ff32209f3853a872e8c40a476
Ben Morrall <bemo56@hotmail.com>
2012-12-13 23:23:14 +1100

    Moved leftover code into admin module

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ Benmorrall::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( jquery.js admin/application.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false

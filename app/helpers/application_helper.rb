faca34900d12372feb64fb4df216243a2f126c0c
Ben Morrall <bemo56@hotmail.com>
2012-12-13 23:21:44 +1100

    Spiderpig now belongs to a div

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def body_class
+    classes = [
+      params[:controller],
+      "#{params[:controller]}_#{params[:action]}"
+    ]
+    classes << "#{params[:controller]}_#{[params[:id]]}" unless params[:id].blank?
+    classes.join(' ')
+  end
+
 end

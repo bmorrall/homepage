29a2a2964c58e03b1a6821518b0d5b759343bcd9
Ben Morrall <bemo56@hotmail.com>
2012-12-23 01:19:51 +1100

    Added HTTP Basic authentication to admin controllers

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,3 +1,4 @@
 class Admin::ApplicationController < ActionController::Base
   protect_from_forgery
+  http_basic_authenticate_with :name => "admin", :password => Settings.password
 end
--- config/initializers/default_settings.rb
+++ config/initializers/default_settings.rb
@@ -1,3 +1,4 @@
 # Default settings go here
 # Setting.defaults[:some_setting] = 'footastic'
-# Settings.default[:analytics_account] = nil # Google Analytics Account ID
+Settings.defaults[:analytics_account] = nil # Google Analytics Account ID
+Settings.defaults[:password] = '!Password' # Password for accounts

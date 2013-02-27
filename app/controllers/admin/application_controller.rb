d47c0c2645e186a70969a37f3f1c82b5a777534b
Ben Morrall <bemo56@hotmail.com>
2013-02-27 05:23:54 +0000

    Added 403 error page for cancan exceptions

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,4 +1,8 @@
 class Admin::ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
+
+  rescue_from CanCan::AccessDenied do |exception|
+    render :file => "#{Rails.root}/public/403.html", :status => 403, :layout => false
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery
 
+  rescue_from CanCan::AccessDenied do |exception|
+    render :file => "#{Rails.root}/public/403.html", :status => 403, :layout => false
+  end
+
   protected
 
   # check content has changed since last_updated

812f45256ca368d7bd52e85e046aae4b1084a05f
Ben Morrall <bemo56@hotmail.com>
2013-02-27 19:43:19 +1100

    Added authorization safety check to admin

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,6 +1,7 @@
 class Admin::ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
+  check_authorization # Ensure CanCan is used for Authorisation
 
   rescue_from CanCan::AccessDenied do |exception|
     render :file => "#{Rails.root}/public/403.html", :status => 403, :layout => false

09f6450c3de2032a0ec5844c625ebe9b2f31a480
bmorrall <ben.morrall@sitepoint.com>
2013-11-21 11:29:57 +1100

    Fixed deprecated devise scope warnings

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Benmorrall::Application.routes.draw do
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" } do
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_scope :users do
     get '/users/auth/:provider' => 'users/omniauth_callbacks#passthru'
   end
 

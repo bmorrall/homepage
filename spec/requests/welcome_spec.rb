bf8796b7b2cdee696c03437a63cf3a5f28b490b8
Ben Morrall <bemo56@hotmail.com>
2013-11-25 22:31:40 +1100

    Added Google Analytics coverage

--- spec/requests/welcome_spec.rb
+++ spec/requests/welcome_spec.rb
@@ -8,6 +8,11 @@ describe "Welcome" do
         get root_url
         response.status.should be(200)
       end
+      it 'renders the Google Analytics content' do
+        Settings.analytics_account = 'UA-12345678'
+        get root_url
+        response.body.should include("_gaq.push(['_setAccount', 'UA-12345678'])")
+      end
     end
   end
 

84504ee63f269ab717046bb8e2d047bbfe35388d
Ben Morrall <bemo56@hotmail.com>
2013-11-25 21:14:56 +1100

    Replaced outdated cancan matcher

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -7,7 +7,7 @@ describe WelcomeController do
       before(:each) { get :index }
       it { should render_template(:index) }
       it { should render_with_layout(:application) }
-      it { should respond_with_content_type(:html) }
+      it { response.content_type.should eq('text/html') }
       it { should_not set_the_flash }
     end
   end

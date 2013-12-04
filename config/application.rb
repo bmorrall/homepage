b964b795c620814e0fa83f81dfbdab662245c5b3
Ben Morrall <bemo56@hotmail.com>
2013-12-04 21:23:06 +1100

    Removed i18n deprecation warning

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,9 @@ module Benmorrall
     # Activate observers that should always be running.
     # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 
+    # Enforce time zone locales
+    config.i18n.enforce_available_locales = true
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'

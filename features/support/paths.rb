ab12fe412ab02dc6f38b277706b6c0f6b5169798
Ben Morrall <bemo56@hotmail.com>
2013-12-09 21:54:36 +1100

    Added cache busting param to homepage url

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -2,7 +2,8 @@ def path_to(page_name)
   case page_name
 
   when /the homepage/
-    root_path
+    # Include a cache busting param
+    root_path(:_ => Random.rand(1000000000))
   else
     raise "Can't find mapping from \"#{page_name}\" to a path."
   end

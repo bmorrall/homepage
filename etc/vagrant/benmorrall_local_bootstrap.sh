0fa14f4ba256cd5b77c0b660aa145c422eb3f068
Ben Morrall <bemo56@hotmail.com>
2013-12-15 16:09:24 +1100

    Added vagrant ruby install script

---
+++ etc/vagrant/benmorrall_local_bootstrap.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+PROJECT_DIR="/webapps/benmorrall.local"
+
+apt-get update
+
+# Install build essentials
+apt-get -y install build-essential git-core
+
+# Install dev essentials
+apt-get install -y vim
+
+# Install rbenv
+git clone git://github.com/sstephenson/rbenv.git /usr/local/rbenv
+ 
+# Add rbenv to the path
+echo '# rbenv setup' > /etc/profile.d/rbenv.sh
+echo 'export RBENV_ROOT=/usr/local/rbenv' >> /etc/profile.d/rbenv.sh
+echo 'export PATH="$RBENV_ROOT/bin:$PATH"' >> /etc/profile.d/rbenv.sh
+echo 'eval "$(rbenv init -)"' >> /etc/profile.d/rbenv.sh
+ 
+chmod +x /etc/profile.d/rbenv.sh
+source /etc/profile.d/rbenv.sh
+
+# Install ruby-build
+pushd /tmp
+  git clone git://github.com/sstephenson/ruby-build.git
+  cd ruby-build
+  ./install.sh
+popd
+ 
+# Install Ruby 1.9.3-p484
+rbenv install 1.9.3-p484
+rbenv global 1.9.3-p484
+
+# Setup Directories
+mkdir /webapps
+ln -fs /vagrant $PROJECT_DIR
+
+# Rehash
+rbenv rehash
+
+# Setup the project
+pushd $PROJECT_DIR
+  gem install bundler
+  bundle install
+  touch tmp/restart.txt
+popd
